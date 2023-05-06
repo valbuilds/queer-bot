ce90e3fe489b620406ecfd82afb961e8f3c1de54
Katie <katie@katiebuilds.website>
2023-05-06 08:42:52 -0500

    forgor

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -8,6 +8,7 @@ module.exports = {
     async execute(interaction) {
         const helpEmbed = new EmbedBuilder()
             .setTitle(`Help`)
+            .setURL(`http://qbot.katiebuilder.xyz/`)
             .addFields(
                 { name: `Utility`, value: '</help:1102412097473761380> - Replies with this message.\n</suggest:1102473783144624178> - Suggest features for the server\n</vent:1104400399462572175> - Got something you need to get off your chest?' },
                 { name: `Silly`, value: `</brisket:1104400399462572172> - brisket best girl\n</hi:1104400399462572173> - say hi!\n</meme:1104400399462572174> - get a random transfem meme.` }
