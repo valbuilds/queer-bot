47b777fa8bdf07c927a79d725a0fbb3aaf09d1f1
Katie <katie@katiebuilds.website>
2023-05-07 09:43:23 -0500

    forgor to update this

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-        client.user.setActivity(`/help・v0.4`, { type: ActivityType.Listening });
+        client.user.setActivity(`/help・v1.0`, { type: ActivityType.Listening });
         console.log('Online!');
 	},
 };
