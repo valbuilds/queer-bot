f5d77e27940d140792abcb4845c7e2b3a67505c8
Katie <katie@katiebuilds.website>
2023-05-08 14:22:37 -0500

    /olivia command

---
+++ commands/goofyandsilly/olivia.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder, EmbedBuilder } = require(`discord.js`);
+
+module.exports = {
+    cooldown: 60,
+    data: new SlashCommandBuilder()
+        .setName(`olivia`)
+        .setDescription(`the real brisket :3`),
+    async execute(interaction) {
+        const user = interaction.user;
+        const emb = new EmbedBuilder()
+            .setImage(`https://cdn.discordapp.com/attachments/876442194377080872/1105211245369507961/56ee21cb4b8e0ed9ed584b481ec7bfb5.png`)
+
+        return interaction.reply({ content: `<@${user.id}>`, embeds: [emb] });
+    }
+}
