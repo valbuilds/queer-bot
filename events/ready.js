64de7e4560f8a50a67ca930f5b7f5701790a5160
Katie <katie@katiebuilds.website>
2023-05-10 02:22:44 -0500

    aaa

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,12 @@
 const { Events, ActivityType } = require('discord.js');
+const { status } = require(`../config.json`)
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-        client.user.setActivity(`/help・v1.1`, { type: ActivityType.Listening });
+        client.user.setActivity(`${status}`, { type: ActivityType.Listening });
         console.log('Online!');
+		console.log(`Status: ${status}`)
 	},
 };
