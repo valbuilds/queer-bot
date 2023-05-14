d7f353b17db2e85dfafec4e7f2fdc412093be7a5
Katie <katie@katiebuilds.website>
2023-05-14 03:41:35 -0500

    yippee

--- commands/utility/deny.js
+++ commands/utility/deny.js
@@ -13,10 +13,6 @@ module.exports = {
             option.setName(`creator`)
                 .setDescription(`Creator(s)`)
                 .setRequired(true))
-        .addBooleanOption(option=>
-            option.setName(`multiple`)
-                .setDescription(`Are there multiple authors?`)
-                .setRequired(true))
         .addBooleanOption(option =>
             option.setName(`explicit`)
                 .setDescription(`Is the song marked as explicit on Spotify?`)
@@ -29,33 +25,46 @@ module.exports = {
         const title = interaction.options.getString(`title`);
         const creator = interaction.options.getString(`creator`);
         const reason = interaction.options.getString(`reason`);
-        const multi = interaction.options.getBoolean(`multiple`);
         const explicit = interaction.options.getBoolean(`explicit`);
-        const acceptedBy = interaction.member;
+        const deniedBy = interaction.member;
+
+        const channel = interaction.channel;
 
         const reply = new EmbedBuilder()
-            .setTitle(`✅ Accepted`)
-            .setDescription(`<@${acceptedBy.id}> has denied this submission.`)
+            .setTitle(`❎ Denied`)
+            .setDescription(`<@${deniedBy.id}> has denied this submission.`)
             .addFields(
                 { name: `Song name`, value: `${title}` },
                 { name: `Song author`, value: `${creator}` },
                 { name: `Reason`, value: `${reason}` },
                 { name: `Marked as explicit by Spotify`, value: `${explicit}` },
             )
-        const replyMulti = new EmbedBuilder()
-            .setTitle(`✅ Accepted`)
-            .setDescription(`<@${acceptedBy.id}> has denied this submission.`)
-            .addFields(
-                { name: `Song name`, value: `${title}` },
-                { name: `Song authors`, value: `${creator}` },
-                { name: `Reason`, value: `${reason}` },
-                { name: `Marked as explicit by Spotify`, value: `${explicit}` },
-            )
 
-        if (multi === `true`) {
-            return interaction.reply({ embeds: [replyMulti] });
-        } else if (multi === `false`) {
-            return interaction.reply({ embeds: [reply] });
+        if (channel.isThread) {
+            isThread();
+        } else {
+            notAThread()
+        }
+
+        async function isThread() {
+            if (!channel.locked) {
+                finalize()
+            } else if (channel.locked) {
+                alreadyDone()
+            }
+        }
+
+        async function finalize() {
+            await channel.setName(`${title} by ${creator}`);
+            await channel.setAppliedTags([`1107150512580734976`]);
+            await interaction.reply({ embeds: [reply] });
+            await channel.setLocked(true);
+        }
+        async function alreadyDone() {
+            return interaction.reply({ content: `This submission has already been accepted.`, ephemeral: true });
+        }
+        async function notAThread() {
+            return interaction.reply({ content: `This channel is not a thread.`, ephemeral: true });
         }
     }
 }
