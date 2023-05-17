becd5cfc62618a32c1962d26ec6cb0f4d31936b1
Katie <katie@katiebuilds.website>
2023-05-17 10:27:31 -0500

    oop

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token, status } = require('./config.json');
-const cron = require(`cron`);
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
