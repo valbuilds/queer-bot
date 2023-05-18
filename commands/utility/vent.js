2eab13333f260ddf1ec5c044cea70aa4c69d2fa1
Katie <katie@katiebuilds.website>
2023-05-18 11:35:10 -0500

    a

--- commands/utility/vent.js
+++ commands/utility/vent.js
@@ -9,12 +9,12 @@ module.exports = {
                 .setDescription(`Talk it out, babe.`)
                 .setRequired(true))
         .addBooleanOption(option =>
-            option.setName(`anon`)
-                .setDescription(`Want to secretly send in a rant? Set this to true.`)
+            option.setName(`anonymous`)
+                .setDescription(`Note: Staff can still see anonymous vents.`)
                 .setRequired(true)),
     async execute(interaction) {
-        const anon = interaction.options.getBoolean(`anon`);
-        const rant = interaction.options.getString(`details`);
+        const anon = interaction.options.getBoolean(`anonymous`);
+        const vent = interaction.options.getString(`details`);
         const user = interaction.user;
         const id = Math.floor(Math.random() * 9999);
         const userPfp = user.avatarURL({
@@ -25,20 +25,28 @@ module.exports = {
 
         const logA = new EmbedBuilder()
             .setAuthor({ name: `⚠️ Messaging a user who sent an anonymous vent without proper reasoning is abuse and is grounds for demotion.` })
-            .setTitle(`Anonymous Rant ID #${id}`)
-            .setDescription(`> ${rant}`)
+            .setTitle(`Anonymous Vent ID #${id}`)
+            .setDescription(`> ${vent}`)
             .setColor(`Gold`)
             .addFields(
                 { name: `Sent by:`, value: `||<@${user.id}>||` },
             )
-        const rantA = new EmbedBuilder()
-            .setTitle(`Anonymous Rant ID #${id}`)
-            .setDescription(`> ${rant}`)
+        const ventA = new EmbedBuilder()
+            .setTitle(`Anonymous Vent ID #${id}`)
+            .setDescription(`> ${vent}`)
             .setColor(`Gold`)
 
-        const rantN = new EmbedBuilder()
-            .setTitle(`Anonymous Rant ID #${id}`)
-            .setDescription(`> ${rant}`)
+        const ventAR = new EmbedBuilder()
+            .setTitle(`Anonymous Vent ID #${id}`)
+            .setDescription(`> ${vent}`)
+            .setColor(`Gold`)
+            .addFields(
+                { name: `Staff can tell who sent an anonymous vent.`, value: `This is only for moderation purposes.\n*If a staff member contatcts you about this, please let a higher up know.*` },
+            )
+
+        const ventN = new EmbedBuilder()
+            .setTitle(`Anonymous Vent ID #${id}`)
+            .setDescription(`> ${vent}`)
             .setColor(`Gold`)
             .setAuthor({ name: `${member.displayName}`, iconURL: `${userPfp}` })
 
@@ -46,14 +54,14 @@ module.exports = {
             const channel = interaction.client.channels.cache.get('1102132112280277052');
             const logChannel = interaction.client.channels.cache.get('1104393174526472212');
             await logChannel.send({ embeds: [logA] });
-            await channel.send({ embeds: [rantA] });
-            return interaction.reply({ content: `Sent!`, embeds: [rantA], ephemeral: true });
+            await channel.send({ embeds: [ventA] });
+            return interaction.reply({ content: `Sent!`, embeds: [ventAR], ephemeral: true });
         }
 
         async function def() {
             const channel = interaction.client.channels.cache.get('1103301352941158421');
-            await channel.send({ embeds: [rantN] });
-            return interaction.reply({ content: `Sent!`, embeds: [rantN], ephemeral: true });
+            await channel.send({ embeds: [ventN] });
+            return interaction.reply({ content: `Sent!`, embeds: [ventN], ephemeral: true });
         }
 
         if (anon) {
